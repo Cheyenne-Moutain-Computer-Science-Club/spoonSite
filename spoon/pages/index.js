59269fc027db0a60a25366094653ad3117d37482
Mike968 <michaelhmehall@gmail.com>
2022-11-30 20:26:10 -0700

    Began project

---
+++ spoon/pages/index.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+
+// export default function Home() {
+// 	return (
+// 		<div>
+// 			<h1>Hello World</h1>
+// 		</div>
+// 	);
+// }
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Component() {
+	const { data: session } = useSession();
+	if (session) {
+		return (
+			<>
+				Signed in as {session.user.email} <br />
+				<button onClick={() => signOut()}>Sign out</button>
+			</>
+		);
+	}
+	return (
+		<>
+			Not signed in <br />
+			<button onClick={() => signIn()}>Sign in</button>
+		</>
+	);
+}
