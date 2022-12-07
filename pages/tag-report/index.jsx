cbce3d8d3c778e67a87ac4f4c31c8e7e273e1453
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-12-07 13:35:37 -0700

    steven

---
+++ pages/tag-report/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../../components/navbar'
+import Report from '../../components/report'
+
+
+export default function tagreport() {
+  return (
+    <div>
+        <NavBar/>
+        <Report/>
+    </div>
+  )
+}
