ded910afa67e49349b098b8d64d49809d364a34d
Mike968 <michaelhmehall@gmail.com>
2022-12-13 16:27:23 -0700

    Initial draft of mobile navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -6,52 +6,158 @@ import Link from "next/link";
 export default function NavBar() {
 	const { data: session } = useSession();
 
-	const [isLoading, setIsLoading] = useState(false);
-	const buttonHandler = () => {
-		setIsLoading((current) => !current);
+	const [mobileMenuState, setMobileMenuState] = useState(false);
+
+	const mobileMenuButtonHandler = () => {
+		setMobileMenuState((current) => !current);
 	};
 
-	let dropdown = false;
-
-	useEffect(() => {
-		dropdown = isLoading;
-	}, [isLoading]);
+	const mobileMenu = (
+		<div class="absolute top-12 w-screen rounded-sm bg-gradient-to-r from-blue-500 to-teal-300">
+			{!session && loginButton}
+			{true && menuItems}
+			{true && (
+				<div className="flex flex-col md:order-2">
+					<ul
+						class="py-1 text-sm text-gray-700 dark:text-gray-200"
+						aria-labelledby="personalDetails"
+					>
+						<br />
+						<li class="py-2 px-4 hover:bg-gray-100">
+							<Link
+								href="/profile"
+								className="flex text-gray-700"
+							>
+								<svg
+									class="mr-2 h-6 w-6"
+									fill="none"
+									stroke="currentColor"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+									></path>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+									></path>
+								</svg>
+								Settings
+							</Link>
+						</li>
+						<li class="flex py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+							<Link
+								href="/profile"
+								className="flex text-gray-700"
+							>
+								<svg
+									class="mr-2 h-6 w-6"
+									fill="none"
+									stroke="currentColor"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+									></path>
+								</svg>
+								My Profile
+							</Link>
+						</li>
+						<hr class="border-white" />
+						<li>
+							<button
+								onClick={() => signOut()}
+								className="flex w-44 py-2 px-4 text-left text-gray-700"
+							>
+								<div class="mr-3 text-red-600">
+									<svg
+										class="h-6 w-6"
+										fill="none"
+										stroke="currentColor"
+										viewBox="0 0 24 24"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="2"
+											d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+										></path>
+									</svg>
+								</div>
+								Log Out
+							</button>
+						</li>
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 
 	return (
-		<nav className="rounded-b border-gray-200 bg-gradient-to-r from-blue-500 to-teal-300 px-2 py-2.5 sm:px-4">
-			<div className="container mx-auto flex flex-wrap items-center justify-between">
-				<Link href="/" className="flex items-center">
-					<img
-						src="/Interact-Club-Official.png"
-						className="mr-3 h-6 sm:h-11"
-						alt="Flowbite Logo"
-					/>
-					<span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-						Spoon Game
-					</span>
-				</Link>
-				{session ? (
-					<UserProfileDropdown />
-				) : (
-					<div className="flex md:order-2">
-						<button
-							type="button"
-							className="mr-3 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"
-							onClick={() => signIn()}
-						>
-							Login
-						</button>
-					</div>
-				)}
+		<nav className="rounded-b border-gray-200 bg-gradient-to-r from-blue-500 to-teal-300 py-2.5 sm:px-4">
+			<div className="container mx-auto hidden flex-wrap items-center justify-between px-2 sm:flex">
+				{logoBlock}
+				{session ? <UserProfileDropdown /> : loginButton}
 				{session && menuItems}
 			</div>
+			<div className="container mx-auto flex flex-wrap items-center justify-between px-2 sm:hidden">
+				{logoBlock}
+				<button
+					className="mr-3 text-gray-700"
+					onClick={mobileMenuButtonHandler}
+				>
+					{mobileMenuState ? (
+						<svg
+							className="h-8 w-8"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M6 18L18 6M6 6l12 12"
+							></path>
+						</svg>
+					) : (
+						<svg
+							className="h-8 w-8"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							></path>
+						</svg>
+					)}
+				</button>
+			</div>
+			{mobileMenuState && mobileMenu}
 		</nav>
 	);
 }
 
 const menuItems = (
 	<div className="order-1 flex w-auto items-center justify-between">
-		<ul className="mt-4 flex flex-col rounded-lg border border-gray-700 p-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:text-sm md:font-medium">
+		<ul className="rounded-lgp-4 mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:border-0 md:text-sm md:font-medium">
 			<li>
 				<Link
 					href="/"
@@ -93,5 +199,30 @@ const menuItems = (
 				</a>
 			</li>
 		</ul>
+	</div>
+);
+
+const logoBlock = (
+	<Link href="/" className="flex items-center">
+		<img
+			src="/Interact-Club-Official.png"
+			className="mr-3 h-6 sm:h-11"
+			alt="Flowbite Logo"
+		/>
+		<span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			Spoon Game
+		</span>
+	</Link>
+);
+
+const loginButton = (
+	<div className="flex md:order-2">
+		<button
+			type="button"
+			className="mr-3 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"
+			onClick={() => signIn()}
+		>
+			Login
+		</button>
 	</div>
 );
