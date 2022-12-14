799ddb11d829c92f3673c46ddeb2745a583b442e
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-12-14 12:54:10 -0700

    Error Handling and a reset button (#35)
    * Button styling
    
    * Autocomplete off
    
    * Functional reset
    
    * Very important image
    
    * Working irrelevant modal
    
    * Amazing error messages
    
    * Unneeded code
    
    * Redundancy removal
    
    * Error handling
    
    * Check for size instead of exists

---
+++ components/errorModal.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function ErrorModal(closeHandler, message) {
+    return (
+        <>
+            <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+                <div className="relative my-6 mx-auto w-auto max-w-2xl">
+                    {/*content*/}
+                    <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
+                        {/*header*/}
+                        <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
+                            <h3 className="text-3xl font-semibold">Uh-oh...</h3>
+                        </div>
+                        {/*body*/}
+                        <div className="relative flex flex-auto flex-row p-6">
+                            <p className="my-4 text-lg leading-relaxed text-slate-500">
+                                {message}
+                            </p>
+                            <img
+                                src={"error.png"}
+                                alt="Ope"
+                                className="h-32 w-32"
+                            />
+                        </div>
+                        {/*footer*/}
+                        <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-6">
+                            <button
+                                className="mr-1 mb-1 rounded bg-emerald-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
+                                type="button"
+                                onClick={closeHandler}
+                            >
+                                Ok
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+        </>
+    );
+}
--- pages/tag-report/index.jsx
+++ pages/tag-report/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useState } from "react";
 import { doc, updateDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
@@ -11,11 +11,14 @@ import {
     getDocs,
 } from "firebase/firestore";
 import NavBar from "../../components/navbar";
+import ErrorModal from "../../components/errorModal.jsx";
 
 const db = getFirestore(app);
 
 export default function TagReport() {
     const [values, setValues] = useState(["", "", "", "", "", "", "", ""]);
+    const [showModal, setShowModal] = useState(false);
+    const [errModalMsg, setErrModalMsg] = useState("An error ocurred");
 
     const { data: session } = useSession();
 
@@ -32,14 +35,32 @@ export default function TagReport() {
             collection(db, "users"),
             where("id", "==", uuid)
         );
-        let victimDoc = (await getDocs(queryVictim)).docs[0];
+        let victimSnap = await getDocs(queryVictim);
+        let victimDoc = victimSnap.docs[0];
+        console.log(victimSnap);
+        console.log("Exists output: " + victimSnap.exists);
 
-        if (taggerDoc.data().outBy != 0 || victimDoc.data().outBy != 0) {
-            // Run if tagger or victim is already tagged
-            alert("invalid");
+        if (taggerDoc.data().outBy != 0) {
+            // Run if tagger is already tagged
+            setErrModalMsg(
+                "It would seem that you are attempting to tag someone, yet you also happen to be tagged. Unfortunately this is not something that you can do."
+            );
+            setShowModal(true);
+        } else if (!(victimSnap.size > 0)) {
+            // Run if ID does not exist
+            setErrModalMsg(
+                "Sorry, but the player ID that you have entered does not exist. Please ensure that you have entered all of the numbers properly."
+            );
+            // console.log(victimSnap.exists);
+            setShowModal(true);
+        } else if (victimDoc.data().outBy != 0) {
+            setErrModalMsg(
+                "It looks like you're trying to tag someone who is already tagged... Unfortunately that is not how this game works. Have a nice day!"
+            );
+            setShowModal(true);
         } else {
             // Run if tagger and victim are not tagged
-
+            alert(3);
             // Append victim to tagger's "kill list"
             const taggerRef = doc(db, "users", taggerDoc.id);
             const newKillList = [...taggerDoc.data().tagged, uuid];
@@ -81,9 +102,14 @@ export default function TagReport() {
         publishTagData(Number(combinedValue));
     };
 
+    // Reset handler - clear text fields
+    const handleReset = () => {
+        setValues(["", "", "", "", "", "", "", ""]);
+    };
+
     const TFAStyleInput = (
         <div name="2FAStyleInputs" className="flex justify-center">
-            <div className="whitespace-nowrap">
+            <div className="overflow-scroll whitespace-nowrap">
                 {values.map((value, index) => (
                     <input
                         key={index}
@@ -91,7 +117,8 @@ export default function TagReport() {
                         value={value}
                         id={`input-${index}`}
                         onChange={(event) => handleChange(event, index)}
-                        className="m-5 w-20 rounded-2xl bg-gray-800 py-6 text-center text-white"
+                        autoComplete="off"
+                        className="m-5 w-20 rounded-2xl border-white bg-gray-800 py-6 text-center text-white"
                     />
                 ))}
             </div>
@@ -108,17 +135,35 @@ export default function TagReport() {
                     </h1>
                 </div>
                 <div>
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit} onReset={handleReset}>
                         {TFAStyleInput}
-                        <button
-                            type="submit"
-                            className="mt-9 rounded bg-indigo-600 py-1 px-3 text-xs text-white hover:bg-indigo-500"
-                        >
-                            Submit
-                        </button>
+                        <div className="mt-10 flex justify-center">
+                            <button
+                                type="submit"
+                                className=" rounded-md bg-indigo-600 py-2 px-8 text-base text-white hover:bg-indigo-500"
+                            >
+                                Submit
+                            </button>
+                            <button
+                                type="reset"
+                                className="ml-7 rounded-md bg-red-500 py-2 px-9 text-base text-white hover:bg-red-400"
+                            >
+                                Reset
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
+            {/* <button
+                className="mr-1 mb-1 rounded bg-pink-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-pink-600"
+                type="button"
+                onClick={() => setShowModal(true)}
+            >
+                Open small modal
+            </button> */}
+            {showModal
+                ? ErrorModal(() => setShowModal(false), errModalMsg)
+                : null}
         </div>
     );
 }
