4328331996c1ed305ed9ab8dc4e83b5281eb8571
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-12-25 19:27:57 -0600

    Title start

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
-import Head from "next/head";
-import Image from "next/image";
 import Footer from "../components/footer";
-``;
-
-import { useSession, signIn, signOut } from "next-auth/react";
 import NavBar from "../components/navbar";
 
 export default function Home() {
-	return (
-		<div className="min-h-screen">
-			<NavBar />
-			{/* <div className="flex-grow">
-				<h1>Infomation here</h1>
-			</div> */}
-			<Footer />
-		</div>
-	);
+    return (
+        <div className="min-h-screen">
+            <NavBar />
+            <div className="text-white">
+                <div className="mt-5 flex justify-center">
+                    <div className="flex flex-col justify-center text-center font-sans">
+                        <h2 className="font-semibold">
+                            Cheyenne Mountain High School
+                        </h2>
+                        <h1 className="text-8xl font-extrabold">Spoon Game</h1>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 }
