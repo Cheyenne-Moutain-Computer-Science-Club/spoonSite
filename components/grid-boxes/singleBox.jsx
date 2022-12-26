f82b9ddc223e65d93c6aee895bd1e6a641e91c5b
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2022-12-26 02:31:28 -0600

    More css magic

--- components/grid-boxes/singleBox.jsx
+++ components/grid-boxes/singleBox.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 export default function SingleBox(stat, num) {
     return (
         <div className="h-40 w-40 rounded-3xl bg-gray-800 shadow-xl">
-            <div className="p-4 align-middle">
+            <div className="flex flex-col p-4">
                 <div>
                     <h1 className="text-5xl font-black">{num}</h1>
                     <h2 className="font-semibold">{stat}</h2>
                 </div>
+
+                {/* <div className="mt-7 flex justify-end">
+                    <Image
+                        src="/icons/external.svg"
+                        alt=""
+                        width={20}
+                        height={20}
+                    />
+                </div> */}
             </div>
         </div>
     );
