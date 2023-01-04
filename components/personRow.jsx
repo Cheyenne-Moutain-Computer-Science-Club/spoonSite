d2bfb438af421b67542725d6bf0dad44b5bb3c67
Mike968 <michaelhmehall@gmail.com>
2023-01-03 19:32:17 -0700

    Stylized old leaderboard

--- components/personRow.jsx
+++ components/personRow.jsx
@@ -3,30 +3,57 @@ export default function PersonRow({ obj, place }) {
 	const image = obj.image;
 	const score = obj.score;
 
+	// return (
+	// 	<ul class="divide-y divide-gray-700">
+	// 		<li class="pb-3 sm:pb-4">
+	// 			<div class="flex items-center space-x-4">
+	// 				<div class="inline-flex items-center text-base font-semibold text-white">
+	// 					{place}
+	// 				</div>
+	// 				<div class="flex-shrink-0">
+	// 					<img
+	// 						class="h-8 w-8 rounded-full"
+	// 						src={image}
+	// 						alt="Profile image"
+	// 					/>
+	// 				</div>
+	// 				<div class="min-w-0 flex-1">
+	// 					<p class="truncate text-sm font-medium text-white">
+	// 						{name}
+	// 					</p>
+	// 					<p class="truncate text-sm text-gray-500 dark:text-gray-400">
+	// 						{score}
+	// 					</p>
+	// 				</div>
+	// 			</div>
+	// 		</li>
+	// 	</ul>
+	// );
+
 	return (
-		<ul class="divide-y divide-gray-700">
-			<li class="pb-3 sm:pb-4">
-				<div class="flex items-center space-x-4">
-					<div class="flex-shrink-0">
-						<img
-							class="h-8 w-8 rounded-full"
-							src={image}
-							alt="Neil image"
-						/>
-					</div>
-					<div class="min-w-0 flex-1">
-						<p class="truncate text-sm font-medium text-white">
-							{name}
-						</p>
-						<p class="truncate text-sm text-gray-500 dark:text-gray-400">
-							{score}
-						</p>
-					</div>
-					<div class="inline-flex items-center text-base font-semibold text-white">
-						{place}
-					</div>
+		<li class="pb-3 sm:pb-4">
+			<div class="flex items-center space-x-4">
+				<div class="inline-flex items-center text-base font-semibold text-white">
+					{place}
 				</div>
-			</li>
-		</ul>
+
+				<div class="flex-shrink-0">
+					<img
+						class="h-8 w-8 rounded-full"
+						src={image}
+						alt="Profile image"
+					/>
+				</div>
+
+				<div class="min-w-0 flex-1">
+					<p class="truncate text-sm font-medium text-white">
+						{name}
+					</p>
+					<p class="truncate text-sm text-gray-500 dark:text-gray-400">
+						{score}
+					</p>
+				</div>
+			</div>
+		</li>
 	);
 }
