a0d9e5e27abab58126737b9d69ac2abbb22e9921
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2023-01-03 20:51:58 -0700

    Aesthetic updates (#53)
    * Increase rules margins
    
    * Instruction box & awful title
    
    * Footer spacing
    
    * triplebox fix
    
    * prep for mid-merge
    
    * Compile fixes
    
    * - Massive margin on footer
    
    * Footer simplification
    
    * Round login button
    
    * Replace class w/ className
    
    * I might have broken something
    
    * Revert "Replace class w/ className"
    
    This reverts commit dbaa5b2563dfd9dfb5fe9031babe9c3492dec7d8.
    
    * Manual revert pt1
    
    * Debugging
    
    * It was one wrong import... (#56)
    
    * Responsive rules
    
    * Fixed stroke-... instances
    
    * Footer login/out changes
    
    * White title
    
    * Fixed outlining

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,76 +1,90 @@
-import { signOut } from "@lib/auth";
+import { signIn, signOut } from "@lib/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, firestore } from "@lib/firebase";
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
-	return (
-		<footer className="relative top-[75vh] rounded-t-lg bg-white p-4 shadow dark:bg-gray-900 md:px-6 md:py-8">
-			<div className="sm:flex sm:items-center sm:justify-between">
-				<Link href="/" className="mb-4 flex items-center sm:mb-0">
-					<img
-						src="/Interact-Club-Official.png"
-						className="mr-3 h-8"
-						alt="Flowbite Logo"
-					/>
-					<span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-						Spoon Game
-					</span>
-				</Link>
-				<ul className="mb-6 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mb-0">
-					<li>
-						<Link
-							href="#"
-							className="mr-4 hover:underline md:mr-6 "
-						>
-							About
-						</Link>
-					</li>
-					<li>
-						<Link href="#" className="mr-4 hover:underline md:mr-6">
-							Privacy Policy
-						</Link>
-					</li>
-					<li>
-						<Link
-							href="#"
-							className="mr-4 hover:underline md:mr-6 "
-						>
-							Licensing
-						</Link>
-					</li>
-					<li>
-						<button
-							onClick={() => signOut()}
-							className="flex w-44 py-2 px-4 text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-						>
-							<div class="mr-3 text-red-600">
-								<svg
-									class="h-6 w-6"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-									></path>
-								</svg>
-							</div>
-							Log Out
-						</button>
-					</li>
-				</ul>
-			</div>
-			<hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
-			<span className="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">
-				© 2022{" "}
-				<Link href="" className="hover:underline">
-					Chyenne Mountain Computer Science Club
-				</Link>
-				. All Rights Reserved.
-			</span>
-		</footer>
-	);
+    // Get logged in status
+    const [user] = useAuthState(auth);
+
+    // Control for sign in/out button in the footer
+    const signButton = !user
+        ? [signIn, "text-green-500", "Log In"]
+        : [signOut, "text-red-600", "Log Out"];
+    // const signButton = [1, "text-green-500", "Log In"];
+
+    return (
+        <footer className="relative mt-16 rounded-t-lg bg-white p-4 shadow dark:bg-gray-900 md:px-6 md:py-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <Link href="/" className="mb-4 flex items-center sm:mb-0">
+                    <img
+                        src="interact-logos/interact-logo-blue.png"
+                        className="mr-3 h-8"
+                        alt="Flowbite Logo"
+                    />
+                    <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+                        Spoon Game
+                    </span>
+                </Link>
+                <div className="mb-6 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mb-0">
+                    <button
+                        onClick={() => signButton[0]()}
+                        className="flex w-44 py-2 px-4 text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                        <div className={`mr-3 ${signButton[1]}`}>
+                            <svg
+                                className="h-6 w-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                                ></path>
+                            </svg>
+                        </div>
+                        {signButton[2]}
+                    </button>
+                </div>
+            </div>
+            <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+            <span className="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+                © 2022{" "}
+                <Link href="" className="hover:underline">
+                    Chyenne Mountain Computer Science Club
+                </Link>
+                . All Rights Reserved.
+            </span>
+            <div className="mt-2 flex justify-center gap-2">
+                {/* Any iconlink placeholder */}
+                <Image
+                    src="/meta-icons/React-Dark.svg"
+                    alt="ReactJS logo"
+                    width={32}
+                    height={32}
+                />
+
+                <Image
+                    src="/meta-icons/Next-Dark.svg"
+                    alt="NextJS logo"
+                    width={32}
+                    height={32}
+                />
+
+                <Image
+                    src="/meta-icons/Tailwind-Dark.svg"
+                    alt="TailwindCSS logo"
+                    width={32}
+                    height={32}
+                />
+            </div>
+        </footer>
+    );
 }
--- pages/rules/index.jsx
+++ pages/rules/index.jsx
@@ -1,111 +1,113 @@
 import NavBar from "../../components/navbar";
 import Footer from "../../components/footer";
+import PageTitle from "../../components/pageTitle";
+
+const emphasis =
+    "underline decoration-blue-500 decoration-2 underline-offset-4";
 
 export default function Rules() {
-	return (
-		<div>
-			<NavBar />
-			<div className="m-5 leading-8 tracking-wider text-gray-300">
-				<h1 className="text-3xl font-black">Rules</h1>
-				<br />
-				<p>
-					All rules are made to protect student safety, learning, and
-					privacy. The rules are, but not limited to the following:
-				</p>
-				<ol className="mx-10 my-5 list-decimal space-y-8 marker:font-black marker:text-white">
-					<li className="underline">
-						All school rules listed in the student handbook apply to
-						this game.
-					</li>
-					<li>
-						Gameplay areas:
-						<ul className="ml-5 list-disc space-y-2">
-							<li>
-								<span className="underline">
-									The following areas are considered “safe
-									zones”
-								</span>
-								&nbsp;and you can not be tagged while in these
-								zones: bathrooms, classrooms, athletic
-								practices, parking lot, locker rooms, library,
-								computer lab, Kiva, the gyms, weight room,
-								field, and pool while class is in session, and
-								anywhere that is not inside the physical school
-								building, except the stadium.
-							</li>
-							<li>
-								<span className="underline">
-									Gameplay will commence in these areas:
-								</span>
-								&nbsp;hallways, cafeteria, in the courtyard, in
-								the gyms while there is no class and at after
-								school events (sports games, etc).
-							</li>
-							<li>
-								If your class is moving from one place in the
-								school to another as a group during class time,
-								gameplay will commence and to avoid being tagged
-								you must hold your spoon in the hallway until
-								you reach your new location (ie. fire drill,
-								moving from classroom to library, assemblies)
-							</li>
-							<li>
-								Classrooms, library, computer lab, and Kiva
-								are&nbsp;
-								<span className="underline">
-									not safe zones
-								</span>
-								&nbsp;before school (up to the beginning of 1st
-								period @ 8:25 am) and after school (starting @
-								3:45 pm) unless it is a Zero-Hour classroom.
-							</li>
-						</ul>
-					</li>
-					<li>
-						There will be no running or yelling; this will result in
-						removal from the game.
-					</li>
-					<li>
-						Spoon related rules:
-						<ul className="ml-5 list-disc space-y-2">
-							<li>
-								<span className="underline">
-									Spoons must be held in your hand and visible
-									to other players,
-								</span>
-								&nbsp;not attached to your body with tape,
-								string, or on clothing (pocket) etc.
-							</li>
-							<li>
-								If you forget to bring or lose your spoon, you
-								will run the risk of being tagged at any time.
-							</li>
-							<li>
-								Only the spoon you are given at the beginning of
-								the game keeps you safe from being tagged.
-							</li>
-							<li>
-								Once you are tagged, please turn-in your spoon
-								to the front office so that we can use them
-								again next year.
-							</li>
-						</ul>
-					</li>
-					<li>
-						Do not attempt to gain an unfair advantage in any way.
-						Doing so will result in removal from the game.
-					</li>
-					<li>
-						Any disagreements among players will result in the
-						removal of both parties.
-					</li>
-					<li>
-						By agreeing to these rules you are giving permission for
-						the use of your picture and name.
-					</li>
-				</ol>
-			</div>
-			<Footer />
-		</div>
-	);
+    return (
+        <div>
+            <NavBar />
+            <div className="mx-5 mt-2 mb-5 leading-8 tracking-wider text-gray-300">
+                <div className="mt-5 ml-7">{PageTitle("Rules")}</div>
+                <br className="my-3" />
+                <p className="mx-6 font-semibold text-gray-200 md:mx-24 lg:mx-48">
+                    All rules are made to protect student safety, learning, and
+                    privacy. The rules are, but not limited to the following:
+                </p>
+                <ol className="mx-8 my-5 list-decimal space-y-8 text-sm marker:font-black marker:text-white md:mx-28 md:text-lg lg:mx-56">
+                    <li className={emphasis}>
+                        All school rules listed in the student handbook apply to
+                        this game.
+                    </li>
+                    <li>
+                        Gameplay areas:
+                        <ul className="ml-5 list-disc space-y-2">
+                            <li>
+                                <span className={emphasis}>
+                                    The following areas are considered “safe
+                                    zones”
+                                </span>
+                                &nbsp;and you can not be tagged while in these
+                                zones: bathrooms, classrooms, athletic
+                                practices, parking lot, locker rooms, library,
+                                computer lab, Kiva, the gyms, weight room,
+                                field, and pool while class is in session, and
+                                anywhere that is not inside the physical school
+                                building, except the stadium.
+                            </li>
+                            <li>
+                                <span className={emphasis}>
+                                    Gameplay will commence in these areas:
+                                </span>
+                                &nbsp;hallways, cafeteria, in the courtyard, in
+                                the gyms while there is no class and at after
+                                school events (sports games, etc).
+                            </li>
+                            <li>
+                                If your class is moving from one place in the
+                                school to another as a group during class time,
+                                gameplay will commence and to avoid being tagged
+                                you must hold your spoon in the hallway until
+                                you reach your new location (ie. fire drill,
+                                moving from classroom to library, assemblies)
+                            </li>
+                            <li>
+                                Classrooms, library, computer lab, and Kiva
+                                are&nbsp;
+                                <span className={emphasis}>not safe zones</span>
+                                &nbsp;before school (up to the beginning of 1st
+                                period @ 8:25 am) and after school (starting @
+                                3:45 pm) unless it is a Zero-Hour classroom.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        There will be no running or yelling; this will result in
+                        removal from the game.
+                    </li>
+                    <li>
+                        Spoon related rules:
+                        <ul className="ml-5 list-disc space-y-2">
+                            <li>
+                                <span className={emphasis}>
+                                    Spoons must be held in your hand and visible
+                                    to other players,
+                                </span>
+                                &nbsp;not attached to your body with tape,
+                                string, or on clothing (pocket) etc.
+                            </li>
+                            <li>
+                                If you forget to bring or lose your spoon, you
+                                will run the risk of being tagged at any time.
+                            </li>
+                            <li>
+                                Only the spoon you are given at the beginning of
+                                the game keeps you safe from being tagged.
+                            </li>
+                            <li>
+                                Once you are tagged, please turn-in your spoon
+                                to the front office so that we can use them
+                                again next year.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Do not attempt to gain an unfair advantage in any way.
+                        Doing so will result in removal from the game.
+                    </li>
+                    <li>
+                        Any disagreements among players will result in the
+                        removal of both parties.
+                    </li>
+                    <li>
+                        By agreeing to these rules you are giving permission for
+                        the use of your picture and name.
+                    </li>
+                </ol>
+            </div>
+            <Footer />
+        </div>
+    );
 }
