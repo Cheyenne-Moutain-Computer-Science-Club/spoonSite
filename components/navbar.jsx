5369fa803fcdfaa36b3c7b50e96f943b9227be97
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2023-01-07 17:29:04 -0700

    Responsiveness

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -12,102 +12,102 @@ export default function NavBar() {
         setMobileMenuState((current) => !current);
     };
 
-	const pagesList = (
-		<div className="order-1 flex w-full items-center justify-between lg:w-auto">
-			<ul className="rounded-lgp-4 mt-4 flex w-full flex-col lg:mt-0 lg:w-auto lg:flex-row lg:space-x-8 lg:border-0 lg:text-sm lg:font-medium">
-				<li>
-					<Link
-						href="/"
-						className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
-					>
-						Home
-						<svg
-							class="h-6 w-6 lg:hidden"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-							></path>
-						</svg>
-					</Link>
-				</li>
-				<li>
-					<Link
-						href="/leaderboard"
-						className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
-					>
-						Leaderboard
-						<svg
-							class="h-6 w-6 lg:hidden"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-							></path>
-						</svg>
-					</Link>
-				</li>
-				<li>
-					<Link
-						href="/rules"
-						className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
-					>
-						Rules
-						<svg
-							class="h-6 w-6 lg:hidden"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth="2"
-								d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
-							></path>
-						</svg>
-					</Link>
-				</li>
-				{user && (
-					<li>
-						<Link
-							href="/tag-report"
-							className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
-						>
-							Tag Someone
-							<svg
-								class="h-6 w-6 lg:hidden"
-								fill="none"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
-								></path>
-							</svg>
-						</Link>
-					</li>
-				)}
-			</ul>
-		</div>
-	);
+    const pagesList = (
+        <div className="order-1 flex w-full items-center justify-between lg:w-auto">
+            <ul className="rounded-lgp-4 mt-4 flex w-full flex-col lg:mt-0 lg:w-auto lg:flex-row lg:space-x-8 lg:border-0 lg:text-sm lg:font-medium">
+                <li>
+                    <Link
+                        href="/"
+                        className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
+                    >
+                        Home
+                        <svg
+                            className="h-6 w-6 lg:hidden"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                            ></path>
+                        </svg>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="/leaderboard"
+                        className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
+                    >
+                        Leaderboard
+                        <svg
+                            className="h-6 w-6 lg:hidden"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+                            ></path>
+                        </svg>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href="/rules"
+                        className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
+                    >
+                        Rules
+                        <svg
+                            className="h-6 w-6 lg:hidden"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                            ></path>
+                        </svg>
+                    </Link>
+                </li>
+                {user && (
+                    <li>
+                        <Link
+                            href="/tag-report"
+                            className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
+                        >
+                            Tag Someone
+                            <svg
+                                className="h-6 w-6 lg:hidden"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
+                                ></path>
+                            </svg>
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
 
     const mobileExpandedMenu = (
         <div className="absolute w-screen rounded-sm bg-gradient-to-r from-blue-500 to-teal-300 lg:hidden">
