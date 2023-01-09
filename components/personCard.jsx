6712caccc4e02632567de44c7bb580eeed1aa6ab
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2023-01-08 17:35:02 -0700

    Error modal update

---
+++ components/personCard.jsx
@@ -0,0 +1,36 @@
+export default function PersonCard({ obj, place }) {
+	const name = obj.name.replace("-Student", "");
+	const image = obj.image;
+	const score = obj.score;
+
+	return (
+		<div className="m-4 rounded-lg border border-gray-700 bg-gray-800 shadow-md">
+			<div className="flex flex-col items-center pb-10 pt-3">
+				<h1 className="p-4">
+					{place == 1 && (
+						<span className="m-2 rounded bg-yellow-200 px-2.5 py-0.5 text-lg font-medium text-yellow-800">
+							1
+						</span>
+					)}
+					{place == 2 && (
+						<span className="m-2 rounded bg-gray-400 px-2.5 py-0.5 text-lg font-medium text-gray-800">
+							2
+						</span>
+					)}
+					{place == 3 && (
+						<span className="m-2 rounded bg-amber-800 px-2.5 py-0.5 text-lg font-medium text-amber-500">
+							3
+						</span>
+					)}
+				</h1>
+				<img
+					className="mb-3 h-24 w-24 rounded-full shadow-lg"
+					src={image}
+					alt={name}
+				/>
+				<h5 className="mb-1 text-xl font-medium text-white">{name}</h5>
+				<span className="text-md text-gray-400">{score}</span>
+			</div>
+		</div>
+	);
+}
