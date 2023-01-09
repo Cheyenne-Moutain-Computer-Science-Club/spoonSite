319a98c9f79148ae452740494b5bc9a85d0ab532
Mike <60718603+Mike968@users.noreply.github.com>
2023-01-08 19:21:29 -0700

    Added teams back in

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -22,7 +22,7 @@ export default function NavBar() {
 					>
 						Home
 						<svg
-							class="h-6 w-6 lg:hidden"
+							className="h-6 w-6 lg:hidden"
 							fill="none"
 							stroke="currentColor"
 							viewBox="0 0 24 24"
@@ -44,7 +44,7 @@ export default function NavBar() {
 					>
 						Leaderboard
 						<svg
-							class="h-6 w-6 lg:hidden"
+							className="h-6 w-6 lg:hidden"
 							fill="none"
 							stroke="currentColor"
 							viewBox="0 0 24 24"
@@ -59,14 +59,36 @@ export default function NavBar() {
 						</svg>
 					</Link>
 				</li>
-				<li>
+				<li className="lg:hidden">
+					<Link
+						href="/team-leaderboard"
+						className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
+					>
+						Team Leaderboard
+						<svg
+							className="h-6 w-6"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+								d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+							></path>
+						</svg>
+					</Link>
+				</li>
+                <li>
 					<Link
 						href="/rules"
 						className="flex justify-between py-2 pl-3 pr-4 text-gray-900 hover:text-white lg:p-0"
 					>
 						Rules
 						<svg
-							class="h-6 w-6 lg:hidden"
+							className="h-6 w-6 lg:hidden"
 							fill="none"
 							stroke="currentColor"
 							viewBox="0 0 24 24"
@@ -89,7 +111,7 @@ export default function NavBar() {
 						>
 							Tag Someone
 							<svg
-								class="h-6 w-6 lg:hidden"
+								className="h-6 w-6 lg:hidden"
 								fill="none"
 								stroke="currentColor"
 								viewBox="0 0 24 24"
